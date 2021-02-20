c171b84945d47aa464be9109240a747df0e206e4
sameghosts <seanfeiner.brown@gmail.com>
2021-02-20 14:10:56 -0800

    branch creation initial psuedo and stub

--- testdb.js
+++ testdb.js
@@ -1,14 +1,18 @@
 const db = require('./models')
 
-db.User.create({
-    name: 'Billy Bob',
-    email: 'test1@test.com',
-    password: '123123123'
-}).then(user =>{
-    console.log('🎃Successfully created a user!')
-    console.log(user)
-    process.exit()
-}).catch(err => {
-    console.log('👹 error creating user:\n${err}')
-    process.exit()
-})
+// ------ Original User Create before hash
+
+// db.User.create({
+//     name: 'Billy Bob',
+//     email: 'test1@test.com',
+//     password: '123123123'
+// }).then(user =>{
+//     console.log('🎃 Successfully created a user!')
+//     console.log(user)
+//     process.exit()
+// }).catch(err => {
+//     console.log('👹 error creating user:\n${err}')
+//     process.exit()
+// })
+
+// -------- 
