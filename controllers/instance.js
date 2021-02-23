884964ccab296e6c45499bf8fccba638101e529f
sameghosts <seanfeiner.brown@gmail.com>
2021-02-23 15:50:55 -0800

    testing and fixes

--- controllers/instance.js
+++ controllers/instance.js
@@ -1,17 +1,31 @@
 //TODO: Finish this controller and plan routes
 
 // Import requirments and dependencies
+const express = require('express')
+const db = require('../models')
+const router = express.Router()
+const passport = require('passport')
 
 // router / controller routes
   // Displaying all user Games / Instances
   // Displaying a particular instance
 
+  // GET and PATCH request to read the game instance and make a search
+router.get('/start',  passport.authenticate('jwt', { session: false }), (req, res) => {
+  db.MatchGame.findByIdAndUpdate({_id: req.body._id})
+  .then(foundGame =>{
+    console.log(foundGame)
+    res.status(200).json({ preferences: foundGame.preference,location: foundGame.location, term: foundGame.term })
+  })
+})
+
+module.exports = router;
   // Showing all the restaurant data
 
   // refactoring logic for data / game state logic
 
   //Writing selections from front end 
-  
+
 //Planning any middlewear
   //TODO: Write non REST logic functions
   
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/', (req,res) =>{
 //controllers
 app.use('/api', require('./controllers/auth')) 
 app.use('/user', require('./controllers/user'))
+app.use('/game', require('./controllers/instance'))
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log(`✅ You are listening to to smooth sounds of Port ${process.env.PORT || 3000}`)
